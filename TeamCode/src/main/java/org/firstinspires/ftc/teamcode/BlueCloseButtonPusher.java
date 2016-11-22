d858f9858d63be35179908b361489d075eb921a0
spaghettification <tkgn15@live.com>
2016-11-22 14:09:09 -0600

    Added Actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCloseButtonPusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCloseButtonPusher.java
@@ -14,14 +14,16 @@ public class BlueCloseButtonPusher extends LinearHardwareMap {
         Calibrate();
         waitForStart();
         Drive(.25, 8, 0, 5, false);
-        //Possibly pause for a second to shoot Ball
-        Turn(.125, 30, false);
-        Drive(.25, 50, 0, 4, false);
-        Turn(.125, 0, false);
+        shootParticle(1);
+        Turn(.125, 51, false);
+        Drive(.25, 66.843, 0, 4, false);
+        Turn(.125, 90, false);
         ButtonPush("Blue");
         setPower(0, 0, 0, 0);
+        Drive(.125,-8)
         Turn(.125,0,false);
-        Drive(.25,18,getIntegratedZValue(),5,false);
+        Drive(.25,48,getIntegratedZValue(),5,false);
+        Turn(.125,90,false);
         ButtonPush("Blue");
         sleep(500);
         Drive(.125, -6, 90, 3, false);
@@ -30,9 +32,27 @@ public class BlueCloseButtonPusher extends LinearHardwareMap {
         Drive(.125, -8, getIntegratedZValue(), 5, false);
         Turn(.125, 150, false);
         Drive(.25,50,getIntegratedZValue(),5,false);
-        //Possibly Shoot Ball
+        shootParticle(1);
 
 
+    }
+    public void shootParticle(int Quantity){
+    for (int i; i<Quantity+1;i++){
+        while (!CatapultStop.ispressed){
+        Catapult.setPower(1);
+        }Catapult.setPower(0);
+        sleep(300);
+        while (CatapultStop.ispressed())Catapult.setPower(1);}Catapult.setPower(0);
+        if (i>1){BallCollection.setPower(1){
+               sleep (300);
+               while (!CatapultStop.ispressed()){
+                   Catapult.setPower(1);
+               }Catapult.setPower(0)
+              BallControl.setPosition(ballControlEngagedPosition);
+                        sleep(2000);
+                        
+        
+}
     }
 }
 
